6d9b74b28e8e06ac501def5f942872947612faa1
Valera <zacepin93v@gmail.com>
2024-07-01 22:40:19 +0300

    new_commits

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from main import BooksCollector
+
+
+@pytest.fixture
+def collection():
+    collection = BooksCollector()
+    return collection
+
+
+def pytest_make_parametrize_id(val):
+    return repr(val)
+
+
+@pytest.fixture
+def collection_five_books(collection):
+    collect = collection
+    books = ['Властелин колец', 'Нужные вещи', 'Собака Баскервиллей', 'Незнайка', 'Стальная крыса']
+    genre = ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+    for i in range(5):
+        collect.add_new_book(books[i])
+
+    for i in range(5):
+        collect.set_book_genre(books[i], genre[i])
+
+    return collect
